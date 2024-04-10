a7d4e2e570a9bcca896576e6a251c1ab60045d47
Maria Lima <127319517+MeduuLima@users.noreply.github.com>
2024-04-10 09:38:35 -0300

    ....

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ const botoes = document.querySelectorAll(".botao")
 
 for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
+    botoes[i.function(){
+        botoes[i].classList.add('ativo');
+    } 
+        
+    ]
 }

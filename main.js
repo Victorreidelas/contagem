b14af319738f2b3e0efa1fbf3b7957af7a2c6473
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-04-16 08:07:02 -0300

    .

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll(".botao")
 for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
     botoes[i].onclick = function(){
-        for(letj=0;j<botoes.length; j++){
+        for(let j=0;j<botoes.length; j++){
        botoes[j].classList.remove("ativo")
     } 
         botoes[i].classList.add("ativo")

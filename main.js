8343444b499be3a76295e3cd23c2dcb6981eff7a
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-04-23 07:59:27 -0300

    rr

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao")
-const textos = document.querySelectorAll()
+const textos = document.querySelectorAll("")
 console.console.log();
 for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
@@ -7,7 +7,7 @@ for(let i =0;i<botoes.length;i++){
         for(let j=0;j<botoes.length; j++){
        botoes[j].classList.remove("ativo")
     } 
-        botoes[i].classList.add("ativo")
-        textos[i].classList.add("ativo")
+        botoes[i].classList.add('ativo')
+        textos[i].classList.add('ativo')
     }
 }

468a9dc51951d362a3a83a398404e2d6ea3588fb
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-04-30 19:32:10 -0300

    d

--- main.js
+++ main.js
@@ -14,12 +14,7 @@ for(let i =0;i<botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll('.contador') 
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00"); 
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo = new Date("2024-10-05T00:00:00"); 
 let tempoatual = new Date()
-
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent = ((tempoObjetivo - tempoatual)/1000)/60/60/24

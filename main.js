e94e1610e8908480fe1c2cf2403c56e089324eaa
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-04-30 07:51:45 -0300

    a

--- main.js
+++ main.js
@@ -14,5 +14,7 @@ for(let i =0;i<botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll('.contador') 
+const tempoObjetivo = new Date("2024-10-05"); 
+
 
 contadores[0].textContent = "Contagem regressiva"

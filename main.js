f38721a44864c943d43134b7f063d66ee2dc5a08
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-04-30 07:53:32 -0300

    d

--- main.js
+++ main.js
@@ -15,6 +15,6 @@ for(let i =0;i<botoes.length;i++){
 
 const contadores = document.querySelectorAll('.contador') 
 const tempoObjetivo = new Date("2024-10-05"); 
+let tempoatual = new Date()
 
-
-contadores[0].textContent = "Contagem regressiva"
+contadores[0].textContent = tempoObjetivo

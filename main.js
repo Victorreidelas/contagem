98ef50e7f56dbb2712de661acfe43f05e05365e8
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-05-14 07:51:21 -0300

    s

--- main.js
+++ main.js
@@ -17,4 +17,16 @@ const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo = new Date("2024-10-06T00:00:00"); 
 let tempoatual = new Date()
 
-contadores[0].textContent = ((tempoObjetivo - tempoatual)/1000)/60/60/24
+contadores[0].textContent = calculaTempo(tempoObjetivo1) 
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoatual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    
+    return dias;
+}
